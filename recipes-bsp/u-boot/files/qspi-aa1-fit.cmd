396c6f3c5ecdb3361a06935e95e1217dae05cab1
Stephen L Arnold <sarnold@vctlabs.com>
2026-05-04 13:11:50 -0700

    fix: dev: adapt boot cmd sources to fpga_fit changes, update vboot config
    * (re)add legacy image support to retain current boot.scr functionality
    * (future) migrate boot.scr bits to uEnv before disabling legacy support
    
    Signed-off-by: Stephen L Arnold <sarnold@vctlabs.com>

--- recipes-bsp/u-boot/files/qspi-aa1-fit.cmd
+++ recipes-bsp/u-boot/files/qspi-aa1-fit.cmd
@@ -1,4 +1,7 @@
-bridge enable
 sf probe
+sf read ${ram_addr_bitstream} ${qspi_offset_addr_bitstream} ${size_bitstream}
+fpga loadmk 0 ${ram_addr_bitstream}:fpga-core-1
+
+bridge enable
 sf read ${loadaddr} ${qspi_offset_addr_kernel} ${size_kernel}
 bootm ${loadaddr}#conf-enclustra-user.dtb#conf-socfpga_enclustra_mercury_qspi_overlay.dtbo
