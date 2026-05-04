4b9f1861d88490ea8353d5af29fa631e3b0e7fb3
Stephen L Arnold <sarnold@vctlabs.com>
2026-05-04 13:11:50 -0700

    fix: dev: adapt boot cmd sources to fpga_fit changes, update vboot config
    * (re)add legacy image support to retain current boot.scr functionality
    * (future) migrate boot.scr bits to uEnv before disabling legacy support
    
    Signed-off-by: Stephen L Arnold <sarnold@vctlabs.com>

--- recipes-bsp/u-boot/files/sd-aa1-fit.cmd
+++ recipes-bsp/u-boot/files/sd-aa1-fit.cmd
@@ -1,3 +1,6 @@
+load mmc 0:1 ${loadaddr} bitstream.itb
+fpga loadmk 0 ${loadaddr}:fpga-core-1
+
 bridge enable
 fatload mmc 0:1 ${loadaddr} image.ub
 bootm ${loadaddr}#conf-enclustra-user.dtb#conf-socfpga_enclustra_mercury_@@UBOOT_CONFIG@@_overlay.dtbo
