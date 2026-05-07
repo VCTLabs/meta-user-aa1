fe565fc239a7719a58a187fb00b89e83f3ff4855
Stephen L Arnold <sarnold@vctlabs.com>
2026-05-07 12:25:27 -0700

    new: dev: add swupdate requirements for update image and u-boot
    * add libubootenv recipe and base config file
    * add update image and swupdate files for devel-image-minimal
    * add A/B root wic image and update sd-aa1 boot.cmd for dual root
    
    Signed-off-by: Stephen L Arnold <sarnold@vctlabs.com>

--- recipes-bsp/u-boot/files/sd-aa1-fit.cmd
+++ recipes-bsp/u-boot/files/sd-aa1-fit.cmd
@@ -1,6 +1,10 @@
+saveenv
+if env exists partition; then echo Booting from mmcblk0p${partition}; else setenv partition 3; echo partition not set, default to ${partition}; fi
+
 load mmc 0:1 ${loadaddr} bitstream.itb
 fpga loadmk 0 ${loadaddr}:fpga-core-1
 
 bridge enable
-fatload mmc 0:1 ${loadaddr} image.ub
+load mmc 0:${partition} ${loadaddr} boot/fitImage
+setenv bootargs "earlycon console=ttyS0,115200 rw rootwait root=/dev/mmcblk0p${partition}"
 bootm ${loadaddr}#conf-enclustra-user.dtb#conf-socfpga_enclustra_mercury_@@UBOOT_CONFIG@@_overlay.dtbo
