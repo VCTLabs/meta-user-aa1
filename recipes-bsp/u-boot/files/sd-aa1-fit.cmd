735c7ae1c8e6d9542fa48dfccf75f53fe3de6037
Stephen L Arnold <sarnold@vctlabs.com>
2026-05-10 12:12:58 -0700

    chg: dev: update configs per upstream best practices, add doc examples
    * enable streaming for A/B root partition updates
    * capture current boot device and bootargs on startup
    * enable u-boot config option for redundant environment handling
    
    Signed-off-by: Stephen L Arnold <sarnold@vctlabs.com>

--- recipes-bsp/u-boot/files/sd-aa1-fit.cmd
+++ recipes-bsp/u-boot/files/sd-aa1-fit.cmd
@@ -1,4 +1,4 @@
-saveenv
+
 if env exists partition; then echo Booting from mmcblk0p${partition}; else setenv partition 3; echo partition not set, default to ${partition}; fi
 
 load mmc 0:1 ${loadaddr} bitstream.itb
@@ -7,4 +7,5 @@ fpga loadmk 0 ${loadaddr}:fpga-core-1
 bridge enable
 load mmc 0:${partition} ${loadaddr} boot/fitImage
 setenv bootargs "earlycon console=ttyS0,115200 rw rootwait root=/dev/mmcblk0p${partition}"
+saveenv
 bootm ${loadaddr}#conf-enclustra-user.dtb#conf-socfpga_enclustra_mercury_@@UBOOT_CONFIG@@_overlay.dtbo
